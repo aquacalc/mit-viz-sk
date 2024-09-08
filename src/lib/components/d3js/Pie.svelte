071b67bdacefc5f08df9e4225cdf7a7995f63dcc
aquacalc <aquacalc@yahoo.com>
2024-09-08 10:45:04 -0400

    hover/click Pie slice

--- src/lib/components/d3js/Pie.svelte
+++ src/lib/components/d3js/Pie.svelte
@@ -1,6 +1,11 @@
 <script>
 	import * as d3 from 'd3';
 
+	export let myPieData;
+	export let selectedIndex = -1;
+
+	// let { myPieData, query = '' } = $props();
+
 	let arcGenerator = d3.arc().innerRadius(20).outerRadius(50);
 
 	let myEndAngleCoeff = 0;
@@ -14,26 +19,24 @@
 
 	// -------------- //
 
-	// const myData = [2, 5, 4, 8, 3, 3, 2, 2];
-  
-  let myData = [
-	{ value: 1, label: "apples" },
-	{ value: 2, label: "oranges" },
-	{ value: 3, label: "mangos" },
-	{ value: 4, label: "pears" },
-	{ value: 5, label: "limes" },
-	{ value: 5, label: "cherries" }
-];
+	// let myPieData = [
+	// 	{ value: 1, label: 'apples' },
+	// 	{ value: 2, label: 'oranges' },
+	// 	{ value: 3, label: 'mangos' },
+	// 	{ value: 4, label: 'pears' },
+	// 	{ value: 5, label: 'limes' },
+	// 	{ value: 5, label: 'cherries' }
+	// ];
 
-	// const tot = myData.reduce((sum, next) => sum + next, 0);
-	const tot = myData.reduce((sum, next) => sum + next.value, 0);
+	// const tot = myPieData.reduce((sum, next) => sum + next, 0);
+	$: tot = myPieData.reduce((sum, next) => sum + next.value, 0);
 
-  console.log(`TOT = ${tot}`)
+	// $: console.log(`TOT = ${tot}`);
 
 	let startAngle = 0;
 	let endAngle = 0;
 
-	let arcsArray = myData.map((a) => {
+	$: arcsArray = myPieData.map((a) => {
 		endAngle = startAngle + (a.value / tot) * 2 * Math.PI;
 		let arcData = {
 			startAngle,
@@ -46,13 +49,14 @@
 
 	// console.log(`ARC array: `, arcsArray);
 
-	const arcsGenerator = arcsArray.map((arc) => arcGenerator(arc));
+	$: arcsGenerator = arcsArray.map((arc) => arcGenerator(arc));
 
 	// console.log(`arcsGenerator: `, arcsGenerator);
 
 	// const myFills = ['green', 'yellow', 'red'];
-  let myFills = d3.scaleOrdinal(d3.schemeTableau10);
+	let myFills = d3.scaleOrdinal(d3.schemeTableau10);
 
+	const handlePathClick = (e) => alert(`e.target.name = ${e.target.name}`);
 
 	// ************** //
 	// let angle = 0;
@@ -69,46 +73,66 @@
 	// -------------- //
 </script>
 
-<!-- <h2>Pie Chart</h2> -->
+<div id="range-angle">
+	<label for="end-angle">
+		End Angle
+		<input
+			type="range"
+			name="end-angle"
+			id="end-angle"
+			bind:value={myEndAngleCoeff}
+			min={-2}
+			max={2}
+			step={0.001}
+		/>
+	</label>
+	{myEndAngle.toFixed(4)} rad ({(myEndAngle * (180 / Math.PI)).toFixed(2)}°)
+</div>
 
-<label for="end-angle">
-	End Angle
-	<input
-		type="range"
-		name="end-angle"
-		id="end-angle"
-		bind:value={myEndAngleCoeff}
-		min={-2}
-		max={2}
-		step={0.01}
-	/>
-</label>
-{myEndAngle.toFixed(2)} rad
 <div class="container">
 	<svg viewBox="-50 -50 100 100">
 		{#each arcsGenerator as myPath, idx}
-			<path d={myPath} fill={myFills(idx)} />
+			<!-- <g transform="translate(100,0) rotate(0)"> -->
+			<!-- <path d={myPath} fill={myFills(idx)} /> -->
+			<!-- </g> -->
+			<g transform={`translate(10,0) rotate(${myEndAngle * (180 / Math.PI)})`}>
+				<path
+					d={myPath}
+					fill={myFills(idx)}
+					class:selected={selectedIndex === idx}
+					on:click={() => (selectedIndex = selectedIndex === idx ? -1 : idx)}
+				/>
+			</g>
 		{/each}
-		<!-- <path d={arc} fill={myEndAngle > 3.14 || myEndAngle < -3.14 ? 'red' : 'green'} /> -->
 
+		<!-- {#each arcs as arc, index}
+			<path d={arc} fill={colors(index)} on:click={(e) => (selectedIndex = index)} />
+		{/each} -->
+
+		<!-- <path d={arc} fill={myEndAngle > 3.14 || myEndAngle < -3.14 ? 'red' : 'green'} /> -->
 		<!-- <circle cx={0} cy={0} r={50} fill="red" /> -->
 		<!-- <path d="M -50 0 A 50 50 0 0 1 50 0 A 50 50 0 0 1 -50 0" fill="blue" /> -->
 	</svg>
 
-  <ul class="legend">
-    {#each myData as {value, label}, idx}
-      <li style="--color: { myFills(idx) }">
-        <span class="swatch"></span>
-        {label} <em>({value})</em>
-      </li>
-    {/each}
-  </ul>
-  
+	<ul class="legend">
+		<!-- {#each myPieData.sort((a, b) => b.label - a.label) as { value, label }, idx} -->
+		<!-- <li style="color: {myFills(myPieData.length - 1 - idx)}"> -->
+		{#each myPieData as { value, label }, idx}
+			<li style="color: {myFills(idx)}">
+				<span class="swatch" class:selected={selectedIndex === idx}>
+					{label} ({value})
+				</span>
+			</li>
+		{/each}
+	</ul>
 </div>
 
 <style>
 	.container {
-		border: 0.5px solid burlywood;
+		display: flex;
+		align-items: center;
+		gap: 2rem;
+		/* border: 0.5px solid burlywood; */
 	}
 
 	svg {
@@ -120,7 +144,64 @@
 		overflow: visible;
 	}
 
+	/* svg:has(path:hover) {
+    fill: yellow;
+  } */
+
+	svg:has(path:hover) {
+		cursor: pointer;
+    opacity: 100%;
+    
+		path:not(:hover) {
+			opacity: 30%;
+		}
+	}
+
 	path {
-    opacity: 0.85;
-  }
+		opacity: 0.65;
+		transition: 300ms;
+	}
+
+	.selected {
+		--color: oklch(60% 45% 0) !important;
+		opacity: 1;
+
+		&:is(path) {
+			fill: var(--color);
+		}
+
+		&:is(span) {
+			color: var(--color);
+			font-weight: 600;
+			/* text-decoration: dashed; */
+		}
+	}
+
+	.legend {
+		flex: 1;
+		list-style: circle;
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(9em, 1fr));
+		border: 0.5px solid #aaa;
+		border-radius: 4px;
+		margin: 0px 2rem;
+		padding: 0.75rem;
+		/* width: 35%; */
+	}
+
+	li {
+		display: flex;
+		/* justify-content: flex-end; */
+		gap: 2rem;
+		font-weight: 200;
+		opacity: 0.85;
+	}
+
+	.swatch {
+		/* aspect-ratio: 1 / 1; */
+	}
+
+	#range-angle {
+		padding-top: 18px;
+	}
 </style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,28 +1,79 @@
 <script>
+	import * as d3 from 'd3';
 	import projects from '$lib/projects.json';
 
-	import Project from '$lib/components/Project.svelte';
+	import Project from '../../lib/components/Project.svelte';
+	import Pie from '$lib/components/d3js/Pie.svelte';
 
-  import Pie from '$lib/components/d3js/Pie.svelte';
-	// console.log('--> ', projects)
+	let query = '';
+
+	let filteredProjects = [];
+	let filteredProjectsAll = [];
+	let x = [];
+	let myPieData = [];
+
+	$: {
+		// Filter TITLE
+		filteredProjects = projects.filter((d) => d.title.toLowerCase().includes(query.toLowerCase()));
+
+		// Filter ALL
+		filteredProjectsAll = projects.filter((d, idx) =>
+			Object.values(d).join('\n').toLowerCase().includes(query.toLowerCase())
+		);
+
+		x = d3.rollups(
+			// filteredProjects,
+			filteredProjectsAll,
+			(n) => n.length,
+			(d) => d.year
+		);
+
+		myPieData = x.map(([label, value]) => ({ label, value })).sort((a, b) => a.label - b.label);
+	}
 </script>
 
 <svelte:head>
 	<title>Projects</title>
 </svelte:head>
 
-<h1>{projects.length} Projects</h1>
+{#if query === ''}
+	<h1 class="num-projects">
+		{filteredProjectsAll.length} Project{filteredProjectsAll.length === 1 ? '' : 's'}
+	</h1>
+{:else}
+	<h1 class="num-projects">
+		{filteredProjectsAll.length} Project{filteredProjectsAll.length === 1 ? '' : 's'} include{filteredProjectsAll.length ===
+		1
+			? 's'
+			: ''} "{query.toLowerCase()}"
+	</h1>
+{/if}
 
-<Pie />
+<label for="query">
+	Search:
+	<input type="search" name="query" id="query" bind:value={query} placeholder="search" />
+</label>
+
+{#if filteredProjectsAll.length > 0}
+	<Pie {myPieData} />
+{:else}
+	<div id="sorry">
+		<h2>Sorry! No articles include "{query}"</h2>
+	</div>
+{/if}
 
 <div class="projects">
-	{#each projects as project}
-		<Project {project}/>
-		<!-- {#each projects as { title, description, image }} -->
-		<!-- <article>
-			<h2>{title}</h2>
-			<img src={image} alt="" />
-			<p>{description}</p>
-		</article> -->
+	{#each filteredProjectsAll as project}
+		<Project {project} hLevel={3} />
 	{/each}
 </div>
+
+<style>
+	.num-projects {
+		font-weight: 300;
+	}
+
+	#sorry {
+		margin-top: 5rem;
+	}
+</style>
